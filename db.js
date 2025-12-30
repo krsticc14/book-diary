efb410f256e0f30a86a223c894a11977baf70438
krsticc14 <aleksandar.krstic021@gmail.com>
2025-12-30 13:31:12 +0100

    Use Railway DATABASE_URL for deployment

--- db.js
+++ db.js
@@ -4,9 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+   connectionString: process.env.DATABASE_URL, // use the URL
+  ssl: { rejectUnauthorized: false },
 });
