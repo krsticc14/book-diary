10855cac8f104dfb9a35d404f3a520e9930842a0
krsticc14 <aleksandar.krstic021@gmail.com>
2026-01-23 19:15:53 +0100

    database fix 3

--- db.js
+++ db.js
@@ -8,6 +8,32 @@ export const db = new Pool({
   ssl: { rejectUnauthorized: false }
 });
 
+async function createTable() {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS books (
+        id SERIAL PRIMARY KEY,
+        title TEXT(255) NOT NULL,
+        author TEXT(255),
+        olid TEXT(50),
+        cover_url TEXT(255),
+        notes TEXT,
+        rating INTEGER,
+        date_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        cover_id INTEGER
+      );
+    `);
+
+    console.log("✅ Table 'books' created successfully!");
+    process.exit();
+  } catch (err) {
+    console.error("❌ Error creating table:", err);
+    process.exit(1);
+  }
+}
+
+createTable();
+
 db.connect()
   .then(() => console.log('Database connected successfully!'))
   .catch((err) => console.error('Database connection failed', err));
