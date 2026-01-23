f906c07bc43ec56ffaaf357e723754aebb04d182
krsticc14 <aleksandar.krstic021@gmail.com>
2026-01-23 15:08:58 +0100

    Database fix

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = new Pool({
-   connectionString: process.env.DATABASE_URL, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT, 
   ssl: { rejectUnauthorized: false }
 });
 
