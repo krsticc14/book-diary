c9ec4340ceba8f5c611233d6be83ce4649ff61b7
krsticc14 <aleksandar.krstic021@gmail.com>
2026-01-24 12:16:59 +0100

    db fix 9

--- db.js
+++ db.js
@@ -8,45 +8,6 @@ export const db = new Pool({
   ssl: { rejectUnauthorized: false },
 });
 
-
-async function createTable() {
-  try {
-    await db.query(`
-      CREATE TABLE IF NOT EXISTS books (
-        id SERIAL PRIMARY KEY,
-        title TEXT NOT NULL,
-        author TEXT,
-        olid TEXT,
-        cover_url TEXT,
-        notes TEXT,
-        rating INTEGER,
-        date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        cover_id INTEGER
-      );
-    `);
-
-    console.log("✅ Table 'books' created successfully!");
-  } catch (err) {
-    console.error("❌ Error creating table:", err);
-  }
-}
-async function fixSchema() {
-  try {
-    await db.query(`
-      ALTER TABLE books
-      ADD COLUMN IF NOT EXISTS date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    `);
-
-    console.log("✅ Column date_added ensured!");
-  } catch (err) {
-    console.error("❌ Schema update failed:", err);
-  }
-}
-
-fixSchema();
-
-createTable();
-
 db.connect()
   .then(() => console.log("Database connected successfully!"))
   .catch((err) => console.error("Database connection failed", err));
